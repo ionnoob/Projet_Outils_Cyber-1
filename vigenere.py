2628bb4f82b4e8b26d312d672cac139f3db23c34
ionnoob <63459661+ionnoob@users.noreply.github.com>
2025-12-08 20:03:17 +0100

    Add Vigenère cipher table creation function

--- vigenere.py
+++ vigenere.py
@@ -1,1 +1,12 @@
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def create_table_vigenere():
+
+    table = {}
+
+    for i, lettre_cle in enumerate(alphabet):
+        ligne_decalee = alphabet[i:] + alphabet[:i]
+        table[lettre_cle] = ligne_decalee
+
+    return table
 
